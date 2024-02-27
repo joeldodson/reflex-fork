9fea5575bd9e1e25635452aaab548843003ff153
Masen Furer <m_github@0x26.net>
2024-02-26 21:15:56 -0800

    XXX: logging client token generation

--- reflex/.templates/web/utils/state.js
+++ reflex/.templates/web/utils/state.js
@@ -66,10 +66,15 @@ export const getToken = () => {
   }
   if (typeof window !== 'undefined') {
     if (!window.sessionStorage.getItem(TOKEN_KEY)) {
-      window.sessionStorage.setItem(TOKEN_KEY, generateUUID());
+      const new_token = generateUUID();
+      console.log("Create new client token: ", new_token);
+      window.sessionStorage.setItem(TOKEN_KEY, new_token);
     }
     token = window.sessionStorage.getItem(TOKEN_KEY);
+    console.log("Retrieved existing client token: ", token);
+    return token;
   }
+  console.log("No window object, cannot retrieve token.")
   return token;
 };
 
